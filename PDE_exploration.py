5f1a97cbbda2a3d0ff438f7d3f871f0a8de82f80
Jennifer Kaiser <65861760+jenniferKaiser21@users.noreply.github.com>
2023-03-02 18:02:43 -0500

    Added Jupyter Notebook files and pdf printouts
    PDF printouts added showing data analysis

---
+++ PDE_exploration.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df_13_14 = pd.read_excel("PDE_DATA_CLEAN/13_14.xlsx")
+df_14_15 = pd.read_excel("PDE_DATA_CLEAN/14_15.xlsx")
+df_15_16 = pd.read_excel("PDE_DATA_CLEAN/15_16.xlsx")
+df_16_17 = pd.read_excel("PDE_DATA_CLEAN/16_17.xlsx")
+df_17_18 = pd.read_excel("PDE_DATA_CLEAN/17_18.xlsx")
+df_18_19 = pd.read_excel("PDE_DATA_CLEAN/18_19.xlsx")
+df_19_20 = pd.read_excel("PDE_DATA_CLEAN/19_20.xlsx")
+df_20_21 = pd.read_excel("PDE_DATA_CLEAN/20_21.xlsx")
+df_21_22 = pd.read_excel("PDE_DATA_CLEAN/21_22.xlsx")
+
+print("test")
